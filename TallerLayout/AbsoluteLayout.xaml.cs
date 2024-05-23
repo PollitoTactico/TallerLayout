cd006a4be21c706c04ea4db91b3c5beacfb47972
José Francisco Sánchez Ortega <jose.sanchez.ortega@udla.edu.ec>
2024-05-23 08:14:31 -0500

    Ya esta todo creo espero este :D

--- TallerLayout/AbsoluteLayout.xaml.cs
+++ TallerLayout/AbsoluteLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class AbsoluteLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ButtonClicked2(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new NavigationPage(new GridLayout()));
+    }
 }
--- TallerLayout/FlexLayoutPage.xaml.cs
+++ TallerLayout/FlexLayoutPage.xaml.cs
@@ -8,22 +8,12 @@ public partial class FlexLayoutPage : ContentPage
 	{
         InitializeComponent();
       
-        for (int i = 0; i < 10; i++)
-        {
-          
-            Image imagen = new Image { Source = "dotnet_bot.png" };
-            Label texto = new Label { Text = $"Elemento {i + 1}" };
+       
+        
+    }
 
-            
-            imagen.GestureRecognizers.Add(new TapGestureRecognizer
-            {
-                Command = new Command(() =>
-                {
-              
-                    Navigation.PushAsync(new AbsoluteLayout());
-                })
-            });
-
-        }
+    private async void OnImageButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushModalAsync(new NavigationPage(new AbsoluteLayout()));
     }
 }
