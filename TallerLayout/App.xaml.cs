772a787fbd414d3a1f9523dc67536b36538f867c
panch <panch@HASMTERGAY>
2024-05-22 23:29:41 -0500

    Agregar archivos de proyecto.

---
+++ TallerLayout/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TallerLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TallerLayout/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TallerLayout
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
